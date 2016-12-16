25e97d13be2ba95acecc66515ce8b903c0bc6647
Enrique Barragan <ebarragan1997@gmail.com>
2016-12-16 11:46:20 -0600

    MVP done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,21 @@
 // Check off todos by clicking
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("done");
 });
 
 // Removing todos
-$("span").on("click", function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(event) {
-  
-})
+// Add todos
+$("input[type='text']").on("keypress", function(event) {
+  var todoText = $(this).val();
+  if (event.which === 13 && todoText !== "") {
+    $("ul").append("<li><span>X</span> "+todoText+"</li>");
+    $(this).val("");
+  }
+});
